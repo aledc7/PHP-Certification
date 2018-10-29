2ba6cdb762f419338becbfe7b1f5d65356cf9952
Alejandro De Castro <29775143+aledc7@users.noreply.github.com>
2018-10-29 14:49:18 -0300

    Update 26 - Date and Time.php

--- 26 - Date and Time.php
+++ 26 - Date and Time.php
@@ -33,7 +33,8 @@ echo "<br>";
 // un util ejemplo es usar esta función time, para actualizar automáticamente el copyright del footer de los sitios web:
 
 
-echo "Todos los derechos reservados  &copy; 2010- " . date("Y");
+echo "Copyright&copy; 2010- " . date("Y")."Ingenea SRL";
+   
 
 
 echo "<br>";
